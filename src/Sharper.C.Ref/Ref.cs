ba3e91171cfda3ca642d97e378b2051c53217b8b
Jeremy Hughes <jeremy@tflite.com>
2015-07-21 16:46:52 +1200

    init

---
+++ src/Sharper.C.Ref/Ref.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sharper.C
+{
+    public sealed class Ref<A>
+    {
+        private readonly object updateLock = new object();
+        private A value;
+
+        internal Ref(A a)
+        {
+            value = a;
+        }
+
+        public A View => value;
+
+        public A Update(Func<A, A> f)
+        {
+            lock (updateLock)
+            {
+                value = f(value);
+                return value;
+            }
+        }
+
+        public void Change(A a)
+        {
+            lock (updateLock)
+            {
+                value = a;
+            }
+        }
+    }
+
+    public static class Ref
+    {
+        public static Ref<A> Mk<A>(A a) => new Ref<A>(a);
+    }
+}
