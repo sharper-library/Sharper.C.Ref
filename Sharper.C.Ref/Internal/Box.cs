7a7f26c04a06355fa896d214ddeb1c035e22af76
Jeremy Hughes <jedahu@gmail.com>
2015-07-30 19:05:55 +1200

    Change namespace

---
+++ Sharper.C.Ref/Internal/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Sharper.C.Data.Internal
+{
+    internal sealed class Box<A>
+    {
+        private readonly object updateLock = new object();
+        private A value;
+
+        public Box(A a)
+        {
+            value = a;
+        }
+
+        public A View => value;
+
+        public A Update(Func<A, A> f)
+        {
+            lock (updateLock)
+            {
+                value = f(value);
+                return value;
+            }
+        }
+
+        public Unit Change(A a)
+        {
+            lock (updateLock)
+            {
+                value = a;
+            }
+            return default(Unit);
+        }
+    }
+}
--- Sharper.C.Ref/Ref.cs
+++ Sharper.C.Ref/Ref.cs
@@ -1,36 +1,22 @@
-﻿using System;
+using System;
+using Sharper.C.Data.Internal;
 
-namespace Sharper.C
+namespace Sharper.C.Data
 {
-    public sealed class Ref<A>
+    public struct Ref<A>
     {
-        private readonly object updateLock = new object();
-        private A value;
+        private readonly Box<A> box;
 
         internal Ref(A a)
         {
-            value = a;
+            box = new Box<A>(a);
         }
 
-        public A View => value;
+        public A View => box.View;
 
-        public A Update(Func<A, A> f)
-        {
-            lock (updateLock)
-            {
-                value = f(value);
-                return value;
-            }
-        }
+        public A Update(Func<A, A> f) => box.Update(f);
 
-        public Unit Change(A a)
-        {
-            lock (updateLock)
-            {
-                value = a;
-            }
-            return default(Unit);
-        }
+        public Unit Change(A a) => box.Change(a);
     }
 
     public static class Ref
